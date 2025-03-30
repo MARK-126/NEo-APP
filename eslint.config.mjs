705385e69238108da2ecf5fccba92747a5164892
Marcos Rio <marcosrio.26@gmail.com>
2025-03-30 16:43:38 -0300

    Update IC settings, improve ESLint rules, and add linting and formatting scripts in package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,14 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: [
+      '**/dist',
+      '**/node_modules',
+      '.next/**/*',
+      '**/.next/**/*',
+      '.yarn/**/*',
+      'tmp/**/*',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
